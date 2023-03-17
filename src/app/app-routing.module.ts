7f3e6a16263ab20daac504b6f1999757eef16ca3
pedro <pedro.jcaceresl@gmail.com>
2023-03-17 15:45:20 -0300

    Added routing - auth and pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,33 +1,30 @@
-import { PagesComponent } from './pages/pages.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { LoginComponent } from './auth/login/login.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: '',
-    component: PagesComponent,
-    children: [
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'grafica1', component: Grafica1Component },
-      { path: 'progress', component: ProgressComponent },
-      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-    ]
-  },
 
-  { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent },
+import { PagesRoutingModule } from './pages/pages-routing.module';
+import { AuthRoutingModule } from './auth/auth-routing.module';
+
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+
+
+
+const routes: Routes = [
+  // path: '/dashboard' PagesRouting
+  // path: '/auth' AuthRouting
+  // path: '/medicos' MedicosRouting
+  // path: '/compras' ComprasRouting
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
 
   { path: '**', component: NopagefoundComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    PagesRoutingModule,
+    AuthRoutingModule
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
---
+++ src/app/pages/pages-routing.module.ts
@@ -0,0 +1,26 @@
+import { PagesComponent } from './pages.component';
+import { ProgressComponent } from './progress/progress.component';
+import { NopagefoundComponent } from '../nopagefound/nopagefound.component';
+import { Grafica1Component } from './grafica1/grafica1.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: PagesComponent,
+    children: [
+      { path: '', component: DashboardComponent },
+      { path: 'grafica1', component: Grafica1Component },
+      { path: 'progress', component: ProgressComponent },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class PagesRoutingModule { }
